b35233f080e88d61afed29aef1260f3facb14757
DartAxis <Filatyev.a.a@gmail.com>
2022-08-09 11:18:15 +0300

    Initial commit

---
+++ src/main/java/ru/dartinc/svahasn/SvahasnApplication.java
@@ -0,0 +1,13 @@
+package ru.dartinc.svahasn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvahasnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SvahasnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/dartinc/svahasn/SvahasnApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dartinc.svahasn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvahasnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
